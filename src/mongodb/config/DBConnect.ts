9c96e3c588fcef49a56ae73cb9feb77de2e6fac4
Rick Alves <henryworkdev.ti@gmail.com>
2023-06-05 01:26:19 -0300

    modificando bd

--- src/mongodb/config/DBConnect.ts
+++ src/mongodb/config/DBConnect.ts
@@ -2,7 +2,7 @@ import { connect, connection } from "mongoose";
 
 export default class DBConnect{    
     static async conectar(){
-        const url = process.env.MONGODB_URL || ""
+        const url = process.env.MONGODB_URL || "mongodb+srv://henryworkdevti:_database123@cluster0.yvb8c6j.mongodb.net/competicao?retryWrites=true&w=majority"
         const conn = await connect(url)
         connection.once("open", () => {
             console.log("Sucesso ao conectar DB!")
