289e80051639c72e115bf701a3c6c14918156919
Henrique Alves <henryworkdev.ti@gmail.com>
2023-06-05 16:29:46 -0300

    ajustando estatiticas

--- src/core/estatisticas/EstatitiscaService.ts
+++ src/core/estatisticas/EstatitiscaService.ts
@@ -17,7 +17,7 @@ export default class EstatisticaService {
     }
 
     static cartoesAmarelos(estatisticas:Estatisticas) {
-        const cartoesOrdenado = estatisticas.gols.sort((a:Cartoes, b:Cartoes) => {
+        const cartoesOrdenado = estatisticas.cartoes_amarelos.sort((a:Cartoes, b:Cartoes) => {
             if (a.cartoes < b.cartoes) {
                 return 1;
             }
@@ -31,7 +31,7 @@ export default class EstatisticaService {
     }
 
     static cartoesVermelhos(estatisticas:Estatisticas) {
-        const cartoesOrdenado = estatisticas.gols.sort((a:Cartoes, b:Cartoes) => {
+        const cartoesOrdenado = estatisticas.cartoes_vermelhos.sort((a:Cartoes, b:Cartoes) => {
             if (a.cartoes < b.cartoes) {
                 return 1;
             }
