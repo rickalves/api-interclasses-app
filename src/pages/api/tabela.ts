dec11a58b145ddb44d2320349146a349dd8141d1
Henrique Alves <henryworkdev.ti@gmail.com>
2023-06-05 09:05:46 -0300

    ajustando API

--- src/pages/api/tabela.ts
+++ src/pages/api/tabela.ts
@@ -1,23 +1,38 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import TabelaService from '@/core/tabela/TabelaService';
+var axios = require('axios');
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
 
-  async function getData() {
-    const res = await fetch('https://api.github.com/users/rickalves');
-    
-    if (!res.ok) {
-      throw new Error('Failed to fetch data');
-    }
-   
-    return res.json();
-  }
+  var data = JSON.stringify({
+    "collection": "tabelas",
+    "database": "competicao",
+    "dataSource": "Cluster0",
+  });
 
-  const dados = await getData()
+  var config = {
+    method: 'post',
+    url: 'https://us-east-1.aws.data.mongodb-api.com/app/data-tdjsz/endpoint/data/v1/action/findOne',
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Request-Headers': '*',
+      'api-key': 'YVjXYwVXCMrQUgWzW9YcLl4uL6n9e24LN3Rqqr1zMnlCciZCspxit1QF2A5tE341',
+    },
+    data: data
+  };
 
-  res.status(200).json(dados)
+  axios(config)
+    .then(function (response: any) {
+      res.status(200).json(response.data)
+    })
+    .catch(function (error: any) {
+      console.log(error);
+    });
+
+
 
 }
